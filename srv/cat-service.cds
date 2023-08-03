d38703e7a2d7e82cfa6e114dec7852e2a82faa36
Dale Bezolli <deilbezolli@gmail.com>
2023-08-03 07:59:58 +0000

    Add crud for todo lists

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -3,7 +3,12 @@ using toDoList from '../db/schema';
 service catalogService @(path: '/todoapi') {
     entity TodoItem as projection on toDoList.TodoItem;
     entity TodoList as projection on toDoList.TodoList;
+
     action createTodoItem(ID : UUID, name : String, completed : Boolean) returns String;
     action updateTodoItem(ID : UUID, name : String, completed : Boolean) returns String;
     action deleteTodoItem(ID : UUID)                                     returns String;
+
+    action createTodoList(ID : UUID, name : String) returns String;
+    action updateTodoList(ID : UUID, name : String) returns String;
+    action deleteTodoList(ID : UUID) returns String;
 }
--- srv/cat-service.js
+++ srv/cat-service.js
@@ -33,8 +33,35 @@ const deleteTodoItem = async (req, TodoItem) => {
     return `Todo with ID ${ID} deleted successfully`;
 };
 
+const createTodoList = async (req, TodoList) => {
+    const { ID, name } = req.data;
+
+    const existingTodoList = await SELECT.one.from(TodoList).where({ ID });
+    if (existingTodoList) return req.error(409, `Todo list with ID ${ID} already exists`);
+
+    await INSERT.into(TodoList).entries([{ ID, name }]);
+    return `Todo list with ${ID} created successfully`;
+}
+
+const updateTodoList = async (req, TodoList) => {
+    const { ID, name } = req.data;
+
+    await UPDATE(TodoList).set({ name }).where({ ID });
+    return `Todo list with ${ID} updated successfully`;
+};
+
+const deleteTodoList = async (req, TodoList) => {
+    const { ID } = req.data;
+    const existingTodoList = await SELECT.one.from(TodoList).where({ ID });   
+
+    if(!existingTodoList) return req.error(404, `Todo with ID ${ID} does not exist`);
+
+    await DELETE.from(TodoList).where({ ID });
+    return `Todo with ID ${ID} deleted successfully`;
+};
+
 module.exports = (srv) => {
-    const { TodoItem } = srv.entities;
+    const { TodoItem, TodoList } = srv.entities;
 
     srv.on("createTodoItem", async (req) => {
         try {
@@ -57,6 +84,30 @@ module.exports = (srv) => {
             return await deleteTodoItem(req, TodoItem);
         } catch (error) {
             return handleErrors('deleting', req, error);
+        }
+    });
+
+    srv.on("createTodoList", async (req) => {
+        try {
+            return await createTodoList(req, TodoList);
+        } catch (error) {
+            return handleErrors('adding', req, error);
+        }
+    });
+
+    srv.on("updateTodoList", async (req) => {
+        try {
+            return await updateTodoList(req, TodoList);
+        } catch (error) {
+            return handleErrors('adding', req, error);
+        }
+    });
+
+    srv.on("deleteTodoList", async (req) => {
+        try {
+            return await deleteTodoList(req, TodoList);
+        } catch (error) {
+            return handleErrors('adding', req, error);
         }
     });
 };
